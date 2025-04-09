96001f5b714af403e9357920329935cfb5421f26
Wagner Gonçalves <wagner.wgos@gmail.com>
2025-04-09 19:14:29 -0300

    chore: migrate project to TypeScript and update file extensions

---
+++ src/assignLanes.ts
@@ -0,0 +1,37 @@
+export interface TimelineItem {
+  id: string;
+  title: string;
+  start: string; // formato: YYYY-MM-DD
+  end: string; // formato: YYYY-MM-DD
+  color?: string;
+}
+
+type Lane = TimelineItem[];
+
+/**
+ * Takes an array of items and assigns them to lanes based on start/end dates.
+ * @returns an array of arrays containing items.
+ */
+function assignLanes(items: TimelineItem[]): TimelineItem[][] {
+  const sortedItems = items.sort(
+    (a, b) => new Date(a.start).getTime() - new Date(b.start).getTime()
+  );
+
+  const lanes: TimelineItem[][] = [];
+
+  function assignItemToLane(item: TimelineItem) {
+    for (const lane of lanes) {
+      if (new Date(lane[lane.length - 1].end) < new Date(item.start)) {
+        lane.push(item);
+        return;
+      }
+    }
+    lanes.push([item]);
+  }
+
+  for (const item of sortedItems) {
+    assignItemToLane(item);
+  }
+
+  return lanes;
+}
